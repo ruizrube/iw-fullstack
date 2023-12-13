f69f832948674c068d7685ba670b621abb5db2c2
Iván Ruiz <ivan.ruiz@uca.es>
2023-12-13 11:46:30 +0100

    Se añade versión en el landingpage

---
+++ src/main/java/es/uca/iw/fullstackwebapp/LandingView.java
@@ -0,0 +1,49 @@
+package es.uca.iw.fullstackwebapp;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import es.uca.iw.fullstackwebapp.user.views.UserActivationView;
+import es.uca.iw.fullstackwebapp.user.views.UserHomeView;
+import es.uca.iw.fullstackwebapp.user.views.UserRegistrationView;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+
+@PageTitle("Inicio")
+@Route(value = "")
+@AnonymousAllowed
+public class LandingView extends VerticalLayout {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+
+    public LandingView() {
+
+    }
+
+
+    @PostConstruct
+    public void init() {
+
+        // Este método se ejecuta después del constructor e inyectar las dependencias (appVersion)
+        add(new H1("Bienvenid@ a la aplicación demo"));
+        add(new H2("Ingeniería Web"));
+        add(new H3("Versión: " + appVersion));
+
+        RouterLink homeLink = new RouterLink("User Home view", UserHomeView.class);
+        add(homeLink);
+
+        RouterLink userRegistrationLink = new RouterLink("User Registration view", UserRegistrationView.class);
+        add(userRegistrationLink);
+
+        RouterLink userActivationLink = new RouterLink("User Activation view", UserActivationView.class);
+        add(userActivationLink);
+    }
+
+}

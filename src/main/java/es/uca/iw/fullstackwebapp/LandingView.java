7023e0d5f4f9766ffefca59d68d38d44f5aad5ee
Iván Ruiz Rube <ivan.ruiz@uca.es>
2023-12-14 18:29:29 +0100

    Update LandingView.java

--- src/main/java/es/uca/iw/fullstackwebapp/LandingView.java
+++ src/main/java/es/uca/iw/fullstackwebapp/LandingView.java
@@ -37,6 +37,7 @@ public class LandingView extends VerticalLayout {
 
         add(new H1("Ingeniería Web"));
         add(new H2("Curso 2023/2024"));
+        add(new H2("Hoy es 14/12/2023"));
         add(new H2("Bienvenid@ a la aplicación demo"));
         add(new H3("Versión: " + appVersion));
 
